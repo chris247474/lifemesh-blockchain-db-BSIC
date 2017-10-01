3ebb2a712fbb89e5fc7fcfcc2929e4a952dda507
coostendorp <coostendorp@vinca.ph>
2017-10-01 23:41:56 +0800

    remove some petstore stuff, add lifemesh SOL's, add map

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,15 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Adoption = artifacts.require("./Adoption.sol");
+var Strings = artifacts.require("./strings.sol");
+var LifeMesh = artifacts.require("./lifemesh.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
   deployer.deploy(SimpleStorage);
-  deployer.deploy(Adoption);
+  deployer.deploy(Strings);
+  deployer.link(Strings, LifeMesh);
+  deployer.deploy(LifeMesh);
 };
